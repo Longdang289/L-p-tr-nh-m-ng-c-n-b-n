b8e365520d88336cc073ea4005d125e1890637da
catarena2608 <thodeptrai11111@gmail.com>
2024-10-10 13:37:29 +0700

    Lan dau tien

---
+++ DoAnLon/client.Designer.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace DoAnLon
+{
+    public partial class client : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_eraser = new System.Windows.Forms.Button();
+            this.btn_Pen = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnReundo = new System.Windows.Forms.Button();
+            this.PanelDraw = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_eraser
+            // 
+            this.btn_eraser.Location = new System.Drawing.Point(183, 10);
+            this.btn_eraser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_eraser.Name = "btn_eraser";
+            this.btn_eraser.Size = new System.Drawing.Size(50, 49);
+            this.btn_eraser.TabIndex = 2;
+            this.btn_eraser.Text = "cục gôm";
+            this.btn_eraser.UseVisualStyleBackColor = true;
+            this.btn_eraser.Click += new System.EventHandler(this.btn_eraser_Click);
+            // 
+            // btn_Pen
+            // 
+            this.btn_Pen.Location = new System.Drawing.Point(129, 10);
+            this.btn_Pen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Pen.Name = "btn_Pen";
+            this.btn_Pen.Size = new System.Drawing.Size(50, 49);
+            this.btn_Pen.TabIndex = 3;
+            this.btn_Pen.Text = "bút";
+            this.btn_Pen.UseVisualStyleBackColor = true;
+            this.btn_Pen.Click += new System.EventHandler(this.btn_Pen_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(297, 14);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(69, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(14, 23);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(50, 45);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(14, 81);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(45, 45);
+            this.btnLoad.TabIndex = 8;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(495, 16);
+            this.btnUndo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(50, 45);
+            this.btnUndo.TabIndex = 10;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // btnReundo
+            // 
+            this.btnReundo.Location = new System.Drawing.Point(549, 16);
+            this.btnReundo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnReundo.Name = "btnReundo";
+            this.btnReundo.Size = new System.Drawing.Size(50, 45);
+            this.btnReundo.TabIndex = 11;
+            this.btnReundo.Text = "Reundo";
+            this.btnReundo.UseVisualStyleBackColor = true;
+            // 
+            // PanelDraw
+            // 
+            this.PanelDraw.Location = new System.Drawing.Point(95, 16);
+            this.PanelDraw.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PanelDraw.Name = "PanelDraw";
+            this.PanelDraw.Size = new System.Drawing.Size(13, 11);
+            this.PanelDraw.TabIndex = 12;
+            this.PanelDraw.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelDraw_Paint);
+            // 
+            // client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 420);
+            this.Controls.Add(this.PanelDraw);
+            this.Controls.Add(this.btnReundo);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btn_Pen);
+            this.Controls.Add(this.btn_eraser);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "client";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button btn_eraser;
+        private Button btn_Pen;
+        private ColorDialog colorDialog1;
+        private TrackBar trackBar1;
+        private Button button1;
+        private Button btnSave;
+        private Button btnLoad;
+        private Button btnUndo;
+        private Button btnReundo;
+        private Panel PanelDraw;
+    }
+}
+
---
+++ DoAnLon/client.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnLon
+{
+    public partial class client : Form
+    {
+        private bool isDrawing = false;
+        private Point lastPoint;
+        private Pen pen;
+        private Color currentColor = Color.Black;
+        private int penSize = 2;
+        private Bitmap drawingBitmap; // Bitmap để vẽ lên
+        private Graphics drawingGraphics; // Graphics liên kết với Bitmap
+        private Stack<Bitmap> undoStack = new Stack<Bitmap>();
+        private Panel panelDraw;
+
+
+        public client()
+        {
+            InitializeComponent();
+
+            // Khởi tạo Panel để vẽ
+            panelDraw = new DoubleBufferedPanel();
+            panelDraw.Dock = DockStyle.Fill; // Panel sẽ chiếm toàn bộ Form
+            this.Controls.Add(panelDraw); // Thêm panelDraw vào Form
+
+            pen = new Pen(currentColor, penSize);
+
+            // Đăng ký sự kiện chuột cho panelDraw (không phải Form)
+            panelDraw.MouseDown += panelDraw_MouseDown;
+            panelDraw.MouseMove += panelDraw_MouseMove;
+            panelDraw.MouseUp += panelDraw_MouseUp;
+            panelDraw.Paint += PanelDraw_Paint;
+            panelDraw.Resize += panelDraw_Resize;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (panelDraw.Width > 0 && panelDraw.Height > 0)
+            {
+                drawingBitmap = new Bitmap(panelDraw.Width, panelDraw.Height);
+                drawingGraphics = Graphics.FromImage(drawingBitmap);
+                drawingGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            }
+        }
+
+        private void btn_Pen_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentColor = colorDialog.Color; // Lưu màu đã chọn
+                }
+            }
+
+            // Thiết lập bút vẽ với màu và kích thước đã chọn
+            pen.Color = currentColor;
+            pen.Width = penSize;
+
+            // Invalidate panel để vẽ lại nếu cần
+            panelDraw.Invalidate();
+
+        }
+
+        private void btn_eraser_Click(object sender, EventArgs e)
+        {
+            pen.Color = panelDraw.BackColor;
+            pen.Width = 10; // Tăng kích thước của tẩy
+        }
+        private void panelDraw_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) // Kiểm tra nút chuột
+            {
+                isDrawing = true;
+                lastPoint = e.Location;
+            }
+        }
+
+        private void panelDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                Point currentPoint = e.Location;
+                drawingGraphics.DrawLine(pen, lastPoint, currentPoint); // Vẽ lên Bitmap của panelDraw
+                lastPoint = currentPoint;
+
+                panelDraw.Invalidate(); // Yêu cầu vẽ lại panelDraw
+            }
+        }
+
+        private void panelDraw_MouseUp(object sender, MouseEventArgs e)     
+        {
+            isDrawing = false;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            penSize = trackBar1.Value;
+            pen.Width = penSize;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawingGraphics.Clear(panelDraw.BackColor); // Xóa toàn bộ Bitmap
+            panelDraw.Invalidate(); // Vẽ lại panel
+        }
+   
+
+  
+        public class DoubleBufferedPanel : Panel
+        {
+            public DoubleBufferedPanel()
+            {
+                this.DoubleBuffered = true;
+                this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+                this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+                this.SetStyle(ControlStyles.UserPaint, true);
+                this.UpdateStyles();
+            }
+        }
+        private void panelDraw_Resize(object sender, EventArgs e)
+        {
+            if (panelDraw.Width > 0 && panelDraw.Height > 0)
+            {
+                Bitmap oldBitmap = drawingBitmap;
+
+                drawingBitmap = new Bitmap(panelDraw.Width, panelDraw.Height);
+                drawingGraphics = Graphics.FromImage(drawingBitmap);
+                drawingGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                if (oldBitmap != null)
+                {
+                    drawingGraphics.DrawImage(oldBitmap, Point.Empty);
+                    oldBitmap.Dispose(); // Giải phóng Bitmap cũ
+                }
+
+                panelDraw.Invalidate(); // Vẽ lại panel
+            }
+        }
+
+   
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Tệp PNG|*.png";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                drawingBitmap.Save(saveDialog.FileName);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Tệp Hình Ảnh|*.png;*.jpg;*.bmp";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                drawingBitmap = new Bitmap(openDialog.FileName);
+                drawingGraphics = Graphics.FromImage(drawingBitmap);
+                panelDraw.Invalidate(); // Vẽ lại panel
+            }
+        }
+        private void SaveState()
+        {
+            undoStack.Push((Bitmap)drawingBitmap.Clone()); // Lưu trạng thái hiện tại
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (undoStack.Count > 0)
+            {
+                drawingBitmap = undoStack.Pop();
+                drawingGraphics = Graphics.FromImage(drawingBitmap);
+                panelDraw.Invalidate(); // Vẽ lại panel
+            }
+        }
+
+            private void PanelDraw_Paint(object sender, PaintEventArgs e)
+            {
+                if (drawingBitmap != null)
+                {
+                    // Vẽ Bitmap lên panelDraw
+                    e.Graphics.DrawImage(drawingBitmap, Point.Empty);
+                }
+            }
+    }
+}
