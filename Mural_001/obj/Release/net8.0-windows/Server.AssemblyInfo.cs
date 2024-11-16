1cfa01f631042d1ccdf90137471213a74b0f46f5
nguyenvantien <nguyenvantien.tech@gmail.com>
2024-11-16 21:30:45 +0700

    Lan 7

--- Mural_001/obj/Release/net8.0-windows/Server.AssemblyInfo.cs
+++ Mural_001/obj/Release/net8.0-windows/Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0602ec7440bedbc3a95a9127d1132580f23302")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
